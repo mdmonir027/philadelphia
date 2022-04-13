ccd049e218e6c61543ec4e04f9180ec295ae9211
Md Monirul Islam <mmislam027@gmail.com>
2022-04-13 23:50:49 +0600

    review type filter added

--- src/pages/Reviews/AllReviews.js
+++ src/pages/Reviews/AllReviews.js
@@ -7,24 +7,48 @@ import Review from './Review';
 import TopMenu from './TopMenu';
 const AllReviews = () => {
   const [allReviews, setAllReviews] = useState([]);
+  const [filterReviews, setFilterReviews] = useState([]);
   const [reviews, setReviews] = useState([]);
+
   const [totalPage, setTotalPage] = useState(1);
   const [itemPerPage] = useState(10);
   const [currentPage, setCurrentPage] = useState(1);
+  const [filterType, setFilterType] = useState('all');
+
+  useEffect(() => {
+    if (filterType === 'all') {
+      setFilterReviews(allReviews);
+      return;
+    }
+    if (filterType === 'positive') {
+      setFilterReviews(allReviews.filter((item) => item.rating >= 4));
+      return;
+    }
+    if (filterType === 'negative') {
+      setFilterReviews(allReviews.filter((item) => item.rating < 3));
+      return;
+    }
+    if (filterType === 'neutral') {
+      setFilterReviews(
+        allReviews.filter((item) => item.rating < 4 && item.rating >= 3)
+      );
+      return;
+    }
+  }, [filterType, allReviews]);
 
   useEffect(() => setAllReviews(ALL_REVIEW_DATA), []);
   useEffect(() => {
-    setTotalPage(Math.ceil(allReviews.length / itemPerPage));
-  }, [itemPerPage, allReviews]);
+    setTotalPage(Math.ceil(filterReviews.length / itemPerPage));
+  }, [itemPerPage, filterReviews]);
 
   useEffect(() => {
     setReviews(
-      allReviews.slice(
+      filterReviews.slice(
         currentPage * itemPerPage - itemPerPage,
         currentPage * itemPerPage
       )
     );
-  }, [allReviews, currentPage, itemPerPage]);
+  }, [filterReviews, currentPage, itemPerPage]);
 
   const nextPage = () => {
     if (currentPage !== totalPage) {
@@ -39,7 +63,7 @@ const AllReviews = () => {
   return (
     <div className='mt-10'>
       <div className='custom-container'>
-        <TopMenu />
+        <TopMenu filterType={filterType} setFilterType={setFilterType} />
         <div className='flex'>
           <div className='w-3/12'>
             <LeftSidebar reviews={allReviews} />
--- src/pages/Reviews/TopMenu.js
+++ src/pages/Reviews/TopMenu.js
@@ -1,21 +1,41 @@
 import React from 'react';
 
-const TopMenu = () => {
+const TopMenu = ({ filterType, setFilterType }) => {
   return (
     <div className='flex justify-between mb-4'>
       <h2 className='font-medium text-gray-900 text-2xl'>Latest Reviews</h2>
       <div>
         <ul className='flex gap-4'>
-          <li className='hover:border hover:cursor-pointer  px-5 py-2 text-sm rounded-md'>
+          <li
+            onClick={() => setFilterType('positive')}
+            className={`hover:cursor-pointer  px-5 py-2 text-sm rounded-md ${
+              filterType === 'positive' ? 'border font-medium' : ''
+            }`}
+          >
             Positive
           </li>
-          <li className='hover:border hover:cursor-pointer px-5 py-2 text-sm rounded-md'>
+          <li
+            onClick={() => setFilterType('neutral')}
+            className={`hover:cursor-pointer px-5 py-2 text-sm rounded-md ${
+              filterType === 'neutral' ? 'border font-medium' : ''
+            }`}
+          >
             Neutral
           </li>
-          <li className='hover:border hover:cursor-pointer px-5 py-2 text-sm rounded-md'>
+          <li
+            onClick={() => setFilterType('negative')}
+            className={`hover:cursor-pointer px-5 py-2 text-sm rounded-md ${
+              filterType === 'negative' ? 'border font-medium' : ''
+            }`}
+          >
             Negative
           </li>
-          <li className='border hover:cursor-pointer font-medium  px-5 py-2 text-sm rounded-md'>
+          <li
+            onClick={() => setFilterType('all')}
+            className={`hover:cursor-pointer  px-5 py-2 text-sm rounded-md ${
+              filterType === 'all' ? 'border font-medium' : ''
+            }`}
+          >
             All
           </li>
         </ul>
