c92a01381fce6932e866770d75d34e9fc0d67ce4
Md Monirul Islam <mmislam027@gmail.com>
2022-04-13 10:14:06 +0600

    update packages and tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+
   theme: {
     screens: {
       xs: '460px',
@@ -13,11 +14,22 @@ module.exports = {
     },
     extend: {
       colors: {
+        authbtn: '#0000FF',
+        pr: '#012257',
+        sr: '#bbbbbb',
+        tr: '#f4f4f4',
+        hr: '#e2edff',
+        nr: '#012257',
+        black: '#000000',
         primary: '#e65d55',
         'primary-black': '#383435',
         'custom-gray': '#8f8f8f',
+        lightBlue: '#0D5FC9',
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
